c8336394b483566157057b23ac21f65ddc4a0377
misty0mist <73884870+misty0mist@users.noreply.github.com>
2021-01-15 21:28:29 +0200

    Update settings.py

--- gusto/settings.py
+++ gusto/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '902w**419b!-v#tk4!^nef=y)*()e^4w7x(96zt#)1+aihu7n2'
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = '902w**419b!-v#tk4!^nef=y)*()e^4w7x(96zt#)1+aihu7n2'
+#SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
